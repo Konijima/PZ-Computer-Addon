4adb1c04b1f225aa0fe9181e701f97d5d0e477e6
Konijima <mathieugermain1991@gmail.com>
2021-11-03 01:25:00 -0400

    Update Main.lua

--- media/lua/client/ComputerTemplateAddon/Main.lua
+++ media/lua/client/ComputerTemplateAddon/Main.lua
@@ -6,13 +6,13 @@ local addon = ComputerMod.CreateAddon("TemplateAddon")
 
 --- Create new ComputerEvent
 addon.ComputerEvents = {
-----{ eventName, { "string", "number", "boolean", "table", "Computer", "IsoObject", "InventoryItem", } },
-	{ "TestEvent", { "string" } },
+----{ eventName:string, { "string", "number", "boolean", "table", "Computer", "IsoObject", "InventoryItem", } },
+    { "TestEvent", { "string" } },
 }
 
 --- Create new BiosSettings
 addon.BiosSettings = {
-----{ key, name, descriptionOn, descriptionOff, default },
+----{ key:string, name:string, descriptionOn:string, descriptionOff:string, default:boolean },
     { "bios1", "Setting 1", "descriptionOn", "descriptionOff", true },
     { "bios2", "Setting 2", "descriptionOn", "descriptionOff", false },
     { "bios3", "Setting 3", "descriptionOn", "descriptionOff", true },
@@ -26,40 +26,43 @@ addon.SoftwareTypes = {
 
 --- Add new files
 addon.FilePack = {
-----{ title, content },
+----{ title:string, content:string },
     { "title 1", "content 1" },
 }
 
 --- Add new games
 addon.GamePack = {
-----{ id, title, year, publisher, genre, boredomModifier, stressModifier, audioName, coverTexture, size, format },
+----{ id:string, title:string, year:string, publisher:string, genre:string, boredomModifier:number, stressModifier:number, audioName:string, coverTexture:string, size:number, format:string },
     { "game_1", "Game 1", "1990", "Konijima", "Debug game", 1.0, 1.0, "default", "default", 10, "all" },
 }
 
 --- Add new softwares
 addon.SoftwarePack = {
-----{ id, title, year, publisher, audioName, coverTexture, size, type },
+----{ id:string, title:string, year:string, publisher:string, audioName:string, coverTexture:string, size:number, type:string },
     { "software1", "Debug Software 1", "year", "publisher", "audioName", "coverTexture", 10, "antivirus" },
 }
 
 
 --- Do stuff when the addon is run
 function addon:Start()
-	print(self.name..": My addon has started!")
+    print(self.name..": My addon has started YAY!")
 
     -- Bind a function to the new event
-	ComputerMod.AddEvent("TestEvent", function(str)
-		print("TestEvent trigged : "..str)
-	end)
+    ComputerMod.AddEvent("TestEvent", function(str)
+        print("TestEvent trigged : "..str)
+    end)
+
+    ---error("Test Start() error")
 
     -- Trigger the new event
-	ComputerMod.TriggerEvent("TestEvent", "my string")
+    ComputerMod.TriggerEvent("TestEvent", "my string")
 end
 
 
 --- Do stuff when the addon update
 function addon:Update()
-	print(self.name..": My addon is updating!")
+    ---print(self.name..": My addon is updating!")
+    ---error("Test Update() error")
 end
 
 
